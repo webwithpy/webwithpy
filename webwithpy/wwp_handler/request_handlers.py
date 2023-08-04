e6f532b07d4e9605b2ae5027178cfe043e030254
SvenKeimpema <keimpje1234@gmail.com>
2023-08-04 10:07:30 +0200

    fix: updated handler so it works with HtmlFiles

--- webwithpy/wwp_handler/request_handlers.py
+++ webwithpy/wwp_handler/request_handlers.py
@@ -1,13 +1,17 @@
 from http.server import BaseHTTPRequestHandler
 from ..wwp_data.html_data import HtmlData
-
+from ..wwp_html import HtmlFile
 
 class HttpHandler(BaseHTTPRequestHandler):
     @classmethod
-    def parse_http_handler_req(cls, handler, method):
+    def add_default_header_to_handler(cls, handler):
         handler.send_response(200)
         handler.send_header('Content-type', 'text/html')
         handler.end_headers()
+
+    @classmethod
+    def parse_http_handler_req(cls, handler, method):
+        cls.add_default_header_to_handler(handler)
 
         # load function that has the req path and is a GET request
         req_func = HtmlData.search_req_paths(handler.path, method)
@@ -16,8 +20,15 @@ class HttpHandler(BaseHTTPRequestHandler):
             handler.wfile.write(b"path not found!")
             return
 
+        func_out = cls._handle_req_func(req_func["func"]())
 
-        handler.wfile.write(str(req_func["func"]()).encode('utf-8'))
+        handler.wfile.write(func_out)
+
+    @classmethod
+    def _handle_req_func(cls, out):
+        if isinstance(out, HtmlFile):
+            return out.__load()
+        return str(out).encode('utf-8')
 
     def do_GET(self):
         self.parse_http_handler_req(self, "GET")
