245bd263dbc987482584426c881e84463c7f0ec0
SvenKeimpema <keimpje1234@gmail.com>
2023-09-29 09:53:59 +0200

    feat: basic idea for ORM, this is not what it is going to be. Refactored ORM most likely will be published next week!

--- webwithpy/orm/base.py
+++ webwithpy/orm/base.py
@@ -2,17 +2,12 @@
 The webwithpy ORM will only work with sqlite and won't be expanded for a while.
 However if you ever want to use a different ORM you can just ignore this one and work with a different ORM.
 """
-from .objects import Field
+from .helpers.SQLHelper import SqlHelper as SQLHelper
+from .objects import Table, Field
 from sqlite3 import dbapi2 as sqlite, Connection
 from pathlib import Path
 from os import PathLike
-from typing import Union, List
-
-
-class Table:
-    def __init__(self, name, fields):
-        self.name = name
-        self.fields = fields or []
+from typing import Union, Dict, List
 
 
 class DB:
@@ -22,7 +17,7 @@ class DB:
 
         self.conn: Connection = None
         self.db_path = db_path
-        self.tables: List[Table] = []
+        self.tables: Dict[str, Table] = {}
 
         self.create_connection()
         self.test_connection()
@@ -31,22 +26,47 @@ class DB:
         self.conn.execute("SELECT 1;")
 
     def create_connection(self):
+        """
+        creates a connection when the database is created
+        """
         # Make sure the db path actually exists
         self.db_path.touch()
-        self.conn = sqlite.connect(str(self.db_path)).cursor()
+        self.conn = sqlite.connect(str(self.db_path))
+        self.conn.row_factory = SQLHelper.dict_factory
+        self.conn = self.conn.cursor()
 
-    def define_table(self, table_name: str, *fields: List[Field]):
-        new_table = Table(table_name, fields)
+    def define_table(self, table_name: str, *fields: Field, migrate: bool = True,):
+        """
+        create table in database based on the table_name and fields
+        """
+        fields = list(fields)
+
+        # TODO: make optional primary key, this is currently impossible
+        prime_field = Field('id', "INTEGER PRIMARY KEY AUTOINCREMENT")
+        prime_field = self.__init_field(table_name, prime_field)
+        fields.insert(0, prime_field)
+
+        new_table = Table(name=table_name, fields=fields, migrate=migrate)
+
         for field in fields:
-            new_table.__setattr__(field.)
-        self.tables.append(new_table)
+            field = self.__init_field(table_name, field)
+
+        self.tables[table_name] = new_table
+
+        if migrate:
+            # remove the table if the table exists
+            self.conn.execute(f"DROP TABLE IF EXISTS {table_name};")
+            # create the table with all of the fields in the table
+            create_table_sql = SQLHelper.create_table_stmt(new_table)
+            self.conn.execute(create_table_sql)
+
+    def __init_field(self, tbl_name: str, field: Field) -> Field:
+        field.conn = self.conn
+        field.table_name = tbl_name
+        return field
 
     def __find_table(self, table_name) -> Table:
-        for table in self.tables:
-            if table.name == table_name:
-                return table
-
-        return None
+        return self.tables[table_name]
 
     def __getattribute__(self, item):
         try:
@@ -54,6 +74,5 @@ class DB:
         except AttributeError as e:
             tbl = self.__find_table(item)
             if tbl is None:
-                print("?")
                 raise e
             return tbl
--- webwithpy/orm/objects.py
+++ webwithpy/orm/objects.py
@@ -1,9 +1,60 @@
+from .helpers.SQLHelper import SqlHelper as SQLHelper
+from sqlite3 import Connection
+from typing import List, Union
+
+
 class Field:
-    def __init__(self, field_name, field_type: str):
+    def __init__(self, field_name: str, field_type: str):
         self.name = field_name
         self.type = field_type
+        self.table_name = ""
+        self.conn: Connection = None
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return f"{self.name}: {self.type}"
 
     def __getattribute__(self, item):
         try:
-            super(Field, self).__getattribute__(item)
+            return super(Field, self).__getattribute__(item)
         except AttributeError as e:
+            raise Exception("Not implemented")
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return SQLHelper.get_fields_by_tbl(self.conn, self.table_name, '=', self.name, other)
+
+    def __ge__(self, other):
+        return SQLHelper.get_fields_by_tbl(self.conn, self.table_name, '>=', self.name, other)
+
+    def __gt__(self, other):
+        return SQLHelper.get_fields_by_tbl(self.conn, self.table_name, '>', self.name, other)
+
+    def __lt__(self, other):
+        return SQLHelper.get_fields_by_tbl(self.conn, self.table_name, '<', self.name, other)
+
+    def __le__(self, other):
+        return SQLHelper.get_fields_by_tbl(self.conn, self.table_name, '<=', self.name, other)
+
+
+class Table:
+    def __init__(self, name: str, fields: List[Field], migrate: bool):
+        self.name = name
+        self.fields: List[Field] = fields or []
+        self.migrate = migrate
+
+    def __getattribute__(self, item):
+        try:
+            return super(Table, self).__getattribute__(item)
+        except AttributeError as e:
+            for field in self.fields:
+                if field.name == item:
+                    return field
+            raise e
+
+    # def __hash__(self):
+    #     return hash(self.name)
