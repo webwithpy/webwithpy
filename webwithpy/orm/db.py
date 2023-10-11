d63ffc521e3138209e48f32e6f804f8d8f4d2985
SvenKeimpema <keimpje1234@gmail.com>
2023-10-11 11:18:34 +0200

    updated sqp into webwithpy, again...

--- webwithpy/orm/db.py
+++ webwithpy/orm/db.py
@@ -16,6 +16,7 @@ def dict_factory(cursor, row):
 class DB:
     conn = None
     cursor = None
+    tables = None
 
     def __init__(self, db_path: Union[Path, str]):
         if DB.conn is None and DB.cursor is None:
@@ -25,17 +26,12 @@ class DB:
             # make sure the db exists
             db_path.touch()
 
-            self.conn = sqlite.connect(db_path)
-            self.conn.row_factory = dict_factory
+            DB.conn = sqlite.connect(db_path)
+            DB.conn.row_factory = dict_factory
 
-            DB.conn = self.conn
-            DB.cursor = self.conn.cursor()
-        else:
-            # if 2 db objects are created we want to make sure we use the same conn
-            self.cursor = DB.cursor
-            self.conn = DB.conn
-
-        self.tables = {}
+            DB.conn = DB.conn
+            DB.cursor = DB.conn.cursor()
+            DB.tables = {}
 
     def create_tables(self):
         id_field = Field(field_type="INTEGER PRIMARY KEY AUTOINCREMENT")
@@ -56,17 +52,17 @@ class DB:
             for field_name, field in table_fields.items():
                 field.field_name = field_name
                 field.table_name = table_name
-                field.cursor = self.cursor
-                field.conn = self.conn
+                field.cursor = DB.cursor
+                field.conn = DB.conn
 
             tbl = Table(
-                self.conn,
-                self.cursor,
+                DB.conn,
+                DB.cursor,
                 table_name,
                 [value for value in table_fields.values()],
             )
 
-            self.tables[table_name] = tbl
+            DB.tables[table_name] = tbl
             self._create_table(table_name, *[field for field in table_fields.values()])
 
     def _create_table(self, table_name: str, *fields: Field):
@@ -74,12 +70,12 @@ class DB:
         for field in fields:
             sql += f"{field.field_name} {field.field_type}, "
         sql = sql[:-2] + ")"
-        self.cursor.execute(sql)
+        DB.cursor.execute(sql)
 
     def __getattribute__(self, item):
         try:
             return super(DB, self).__getattribute__(item)
         except Exception as e:
-            if item in self.tables.keys():
-                return self.tables[item]
+            if item in DB.tables.keys():
+                return DB.tables[item]
             raise e
