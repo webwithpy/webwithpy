41205139516049d45aa6e20c1376000c3089a7ab
Sven Keimpema <keimpje1234@gmail.com>
2023-12-01 10:49:57 +0100

    poetry fixing

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 from Cython.Build import cythonize
+from distutils.command.build_ext import build_ext
 
 setup(
-    name='cacher',
-    ext_modules=cythonize("webwithpy/orm/tools/cacher.pyx"),
+    name="cacher",
+    ext_modules=cythonize(
+        "webwithpy/orm/tools/cacher.pyx", compiler_directives={"linetrace": True}
+    ),
+    cmdclass={"build_ext": build_ext},
 )
