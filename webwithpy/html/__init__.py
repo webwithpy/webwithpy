1d6839e1cb0a00c2571611a1665d56b2ed24e3f9
SvenKeimpema <keimpje1234@gmail.com>
2023-12-05 10:48:45 +0100

    easier use of forms and pyhtml

--- webwithpy/html/__init__.py
+++ webwithpy/html/__init__.py
@@ -1,4 +1,5 @@
 from .lexer import Lexer
 from .parser import DefaultParser
 from .renderer import DefaultRenderer
-from .forms import SQLForm
+from .forms import SQLForm, InputForm
+from .pyhtml import Input, Span, Div, H4, H3, P, A, Form, Label
