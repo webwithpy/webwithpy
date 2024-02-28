e1a876b5fd04c2334345b2b58c36807b1778c33c
Sven Keimpema <61794662+SvenKeimpema@users.noreply.github.com>
2024-02-28 16:13:49 +0100

    Added the cookie jar and upgraded request and response for easier use

---
+++ webwithpy/http/cookies.py
@@ -0,0 +1,23 @@
+from typing import Dict, Any
+
+
+class CookieJar:
+    def __init__(self):
+        self.cookies: Dict[str, Any] = {}
+
+    def get_http_cookies(self):
+        response = ""
+        for name, value in self.cookies.items():
+            response += f"Set-Cookie: {name}={value}; Max-Age: 86400; Secure\n"
+
+    def add_cookie(self, name: str, value: Any):
+        if isinstance(value, str):
+            value = value.replace('\r', '')
+
+        self.cookies[name] = value
+
+    def remove_cookie(self, name: str):
+        del self.cookies[name]
+
+    def get_cookie(self, name: str):
+        return self.cookies.get(name)
--- webwithpy/http/response.py
+++ webwithpy/http/response.py
@@ -1,5 +1,7 @@
 from typing import Union, Any
 from os import PathLike
+
+from .cookies import CookieJar
 from ..html.lexer import Lexer
 from ..html.parser import DefaultParser
 from ..html.renderer import DefaultRenderer
@@ -17,10 +19,38 @@ class Response:
         }
 
         self.contents = []
-        self.cache = {}
-        self.cookies = {"session": session}
 
-    def add_content(self, content: Any, template: Union[str, PathLike] = ""):
+        # cached templates for faster loading times and less server stress
+        self._cache = {}
+
+        # Make sure we create the cookie jar and always include the user's session
+        # _ means the variable is 'private'(private vars don't exist however they do in code editors like pycharm!)
+        self._cookie_jar = CookieJar()
+        self._cookie_jar.add_cookie("session", session)
+
+    def get_all_cookies(self):
+        """
+        Get all cookies from the cookie jar
+        """
+        return self._cookie_jar.cookies.items()
+
+    def get_cookie(self, name: str) -> Any:
+        """
+        Get a cookie from the cookie jar
+        """
+        return self._cookie_jar.get_cookie(name)
+
+    def remove_cookie(self, name: str):
+        """
+        Remove a cookie from the cookie jar, so it won't be used in the http request
+        """
+        self._cookie_jar.remove_cookie(name)
+
+    def add_cookie(self, cookie_name: str, cookie_value):
+        """Exposes the cookie jar add_cookie method so users can use it"""
+        self._cookie_jar.add_cookie(cookie_name, cookie_value)
+
+    def _add_content(self, content: Any, template: Union[str, PathLike] = ""):
         """
         function will add any content given the user based on if he gave-up a template
         """
@@ -29,44 +59,40 @@ class Response:
                 content = {}
 
             rendered = (
-                self.parse_template(template, **content)
-                if template not in self.cache
-                else DefaultRenderer.render_pre(self.cache[template], **content)
+                self._parse_template(template, **content)
+                if template not in self._cache
+                else DefaultRenderer.render_pre(self._cache[template], **content)
             )
             self.contents.append(rendered)
         else:
             self.contents.append(str(content))
 
-    def parse_template(self, template: Union[str, PathLike], **kwargs):
+    def _parse_template(self, template: Union[str, PathLike], **kwargs):
         """
-        will parse the template with htpyp
+        Parses the given template with htpyp
         """
         lexer = Lexer()
         tokens = lexer.lex_file(template)
         parser = DefaultParser(tokens)
         program = parser.parse()
         code = DefaultRenderer.generate_pre_code(program)
-        self.cache[template] = code
+        self._cache[template] = code
 
         return DefaultRenderer.render_pre(code, **kwargs)
 
-    def add_header(self, header_name, header_value):
+    def _add_header(self, header_name, header_value):
         """
         Example header_name = Content-Type and header_value = text/html
         """
         self.headers[header_name] = header_value
 
-    def add_cookie(self, cookie_name, cookie_value):
-        """sets a cookie that will be added when building and encoding the resp"""
-        self.cookies[cookie_name] = cookie_value
-
-    def encode(self):
+    def _encode(self):
         """
         encode the response with utf-8, so we can send it over http/https
         """
-        return self.build_response().encode("utf-8")
+        return self._build_response().encode("utf-8")
 
-    def build_response(self) -> str:
+    def _build_response(self) -> str:
         """
         builds an HTTP request that we can send to the user.
         """
@@ -77,19 +103,15 @@ class Response:
         for k, v in self.headers.items():
             response += f"{k}: {v}\n"
 
-        for k, v in self.cookies.items():
-            self.cookies[k] = str(v).replace("\r", "")
-
-        for name, value in self.cookies.items():
-            # Max-Age of all cookies is currently required, this will be change after 1.0
-            response += f"Set-Cookie: {name}={value}; Max-Age=86400\n"
+        # automatically generate cookies for the user
+        response += self._cookie_jar.get_http_cookies()
 
         response += "\n\n"
         response += "\n".join(self.contents)
 
         return response
 
-    def generate_error(self, code=500):
+    def _generate_error(self, code=500):
         """
         the only errors we currently support is or 500 or 404
         """
